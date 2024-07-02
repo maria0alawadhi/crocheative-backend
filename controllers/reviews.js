19b173b6b26183eee4476ae326b09b1ee23260c0
jenanalawadhi1 <jenanalawadhi2@gmail.com>
2024-07-02 17:12:05 +0300

    done with cart and review

---
+++ controllers/reviews.js
@@ -0,0 +1,65 @@
+const mongodb = require('mongodb')
+const { Review } = require('../models')
+const { Item } = require('../models')
+
+const index = async (req, res) => {
+  try {
+    const reviews = await Review.find({})
+    res.send(reviews)
+  } catch (error) {
+    throw error
+  }
+}
+
+const create = async (req, res) => {
+  try {
+    const review = await Review.create(req.body)
+    // adding the review to the Item
+    const item = await Item.findById(req.params.item_id)
+    item.reviews.push(review._id)
+    await item.save()
+
+    res.send(review)
+  } catch (error) {
+    throw error
+  }
+}
+
+const updateReview = async (req, res) => {
+  try {
+    const review = await Review.findByIdAndUpdate(
+      req.params.review_id,
+      req.body,
+      {
+        new: true
+      }
+    )
+    res.send(review)
+  } catch (error) {
+    throw error
+  }
+}
+
+const deleteReview = async (req, res) => {
+  try {
+    await Review.deleteOne({ _id: req.params.review_id })
+    //delete the reference
+    await Item.findByIdAndUpdate(req.params.post_id, {
+      $pull: { reviews: req.params.review_id }
+    })
+    res.send({
+      msg: 'Review Deleted',
+      payload: req.params.review_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  index,
+  create,
+  update: updateReview,
+  delete: deleteReview
+}
---
+++ routes/reviews.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var router = express.Router()
+const ReviewCtrl = require('../controllers/review')
+const middleware = require('../middleware')
+
+//show all reviews
+router.get(
+  '/items/:item_id/reviews',
+  middleware.stripToken,
+  middleware.verifyToken,
+  ReviewCtrl.index
+)
+//create new review
+router.post(
+  '/items/:item_id/reviews',
+  middleware.stripToken,
+  middleware.verifyToken,
+  ReviewCtrl.create
+)
+// update a review
+router.put(
+  '/items/:item_id/reviews/:review_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  ReviewCtrl.update
+)
+//delete a review
+router.delete(
+  '/items/:item_id/reviews/:review_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  ReviewCtrl.delete
+)
+
+module.exports = router
